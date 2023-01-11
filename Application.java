c0c0ed01b3865f8dde060d93e2f759e9939495e0
JustAung <112411778+JustAung@users.noreply.github.com>
2023-01-11 13:20:04 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author aung
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		
+	}
+
+}
